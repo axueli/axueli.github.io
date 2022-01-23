2e29ffc51100f54b008a55a91cd81ca944b020df
axueli <2763587526@qq.com>
2022-01-23 09:50:46 +0800

    Site updated: 2022-01-23 09:50:40

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,12 +2,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+         $('[rel="icon"]').attr('href', "/favicon.png");
          document.title = '八嘎！你要去哪啊！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+         $('[rel="icon"]').attr('href', "/favicon.png");
          document.title = '不要丢下雪梨 QAQ ！好不好' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
